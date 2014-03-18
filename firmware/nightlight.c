e6d5e37a52dba03b1d44085dd2f2371d87fed73f
Shane Gough <shane@thegaragelab.com>
2014-03-19 09:35:53 +1000

    Main program now implements a basic 'blinky' loop controlling a LED on PB1.

--- firmware/nightlight.c
+++ firmware/nightlight.c
@@ -7,13 +7,53 @@
 #include <stdbool.h>
 #include <avr/io.h>
 #include "softuart.h"
+#include "ticker.h"
+
+//---------------------------------------------------------------------------
+// Status LED control
+//---------------------------------------------------------------------------
+
+#define LED_PIN PINB1
+
+/** Initialise the LED
+ */
+static void ledInit() {
+  PORTB |= (1 << LED_PIN);
+  DDRB |= (1 << LED_PIN);
+  }
+
+/** Change the LED state
+ *
+ * @param state true if the LED should be on, false if not
+ */
+static void ledState(bool state) {
+  if(state)
+    PORTB |= (1 << LED_PIN);
+  else
+    PORTB &= ~(1 << LED_PIN);
+  }
+
+//---------------------------------------------------------------------------
+// Main program
+//---------------------------------------------------------------------------
 
 /** Program entry point
  */
 int main() {
-  // TODO: Initialise IO pins
+  uint16_t now;
+  // Initialise IO pins libraries
+  ledInit();
+  tickInit();
+  uartInit();
   // Enter main loop
-  while(true);
+  while(true) {
+    now = ticks();
+    while(tickElapsed(now)<10);
+    ledState(false);
+    now = ticks();
+    while(tickElapsed(now)<10);
+    ledState(true);
+    }
   return 0;
   }
 
