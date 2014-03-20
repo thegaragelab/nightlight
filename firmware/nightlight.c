f4259c965e5ff000d8a039309bc2824e89ca38f6
Shane Gough <shane@thegaragelab.com>
2014-03-20 16:06:55 +1000

    Added initial code for reading ADC channels.

--- firmware/nightlight.c
+++ firmware/nightlight.c
@@ -35,6 +35,50 @@ static void ledState(bool state) {
   }
 
 //---------------------------------------------------------------------------
+// ADC Sampling
+//---------------------------------------------------------------------------
+
+/** Voltage channel (PB3/ADC3) */
+#define CHAN_VOLTAGE ((1 << MUX0) | (1 << MUX1))
+
+/** Motion channel (PB4/ADC2) */
+#define CHAN_MOTION (1 << MUX1)
+
+/** Channel mask */
+#define CHAN_MASK ((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0))
+
+/** Initialise the ADC channels
+ */
+static void adcInit() {
+  ADMUX = (1 << ADLAR) | CHAN_VOLTAGE;
+  ADCSRA = (1 << ADEN);
+  DIDR0 = (1 << ADC2D) | (1 << ADC3D);
+  }
+
+/** Read the given ADC channel
+ *
+ * @return the current reading of the voltage monitoring ADC
+ */
+static uint8_t adcRead(uint8_t channel) {
+  // Select the channel and start the sampling
+  ADMUX = (ADMUX & ~CHAN_MASK) | channel;
+  ADCSRA |= (1 << ADSC);
+  // Wait for the sample to complete
+  while(!(ADCSRA&(1 << ADIF)));
+  // Grab the result and clear the completion flag
+  uint8_t result = ADCH;
+  ADCSRA |= (1 << ADIF);
+  // All done
+  return result;
+  }
+
+/** Helper macro to read the voltage */
+#define adcVoltage() adcRead(CHAN_VOLTAGE)
+
+/** Helper macro to read the motion sensor */
+#define adcMotion() adcRead(CHAN_MOTION)
+
+//---------------------------------------------------------------------------
 // Main program
 //---------------------------------------------------------------------------
 
@@ -44,12 +88,17 @@ int main() {
   uint16_t now;
   // Initialise libraries
   configInit();
+  adcInit();
   ledInit();
   tickInit();
   uartInit();
   // Enter main loop
   while(true) {
+    // Get current time stamp
     now = ticks();
+    // Update sensor values
+    configWrite(STATE_POWER, adcVoltage());
+    configWrite(STATE_MOTION, adcMotion());
     while(tickElapsed(now)<10);
     ledState(false);
     now = ticks();
