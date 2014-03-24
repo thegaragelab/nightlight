7b3e6d00ba917ee701fadcf61f50a74a17d9cc5d
Shane Gough <shane@thegaragelab.com>
2014-03-24 11:35:34 +1000

    Added command processing (GET, SET & SAVE)

--- firmware/nightlight.c
+++ firmware/nightlight.c
@@ -80,13 +80,58 @@ static uint8_t adcRead(uint8_t channel) {
 #define adcMotion() adcRead(CHAN_MOTION)
 
 //---------------------------------------------------------------------------
+// Command processing
+//---------------------------------------------------------------------------
+
+/** Mask to extract command verb */
+#define MASK_COMMAND 0xF000
+
+/** Available commands and responses
+ */
+typedef enum {
+  CMD_GET    = 0x1000, //! Get the value of a configuration/state register
+  CMD_SET    = 0x2000, //! Set the value of a configuration/state register
+  CMD_SAVE   = 0x3000, //! Save configuration registers to EEPROM
+  STATUS_OK  = 0x0000, //! The 'OK' state
+  STATUS_INF = 0x1000, //! 'OK' state with information
+  STATUS_ERR = 0xFFFF, //! The 'ERR' state
+  };
+
+/** The 'GET' verb - used to
+/** Process an incoming command
+ *
+ * @param command the incoming command to process
+ *
+ * @return the result to return for the command.
+ */
+static uint16_t processCommand(uint16_t command) {
+  uint16_t result = STATUS_ERR;
+  uint16_t cmd = command & MASK_COMMAND;
+  if(cmd==CMD_GET) {
+    // Fetch the value of a register
+    result = STATUS_INF | (command & 0x0F00) | (configRead((command >> 8) & 0x0F));
+    }
+  else if(cmd==CMD_SET) {
+    // Set the value of a register
+    configWrite((command >> 8) & 0x0F, command & 0x00FF);
+    result = STATUS_INF | (command & 0x0F00) | (configRead((command >> 8) & 0x0F));
+    }
+  else if(cmd==CMD_SAVE) {
+    // Save registers to EEPROM
+    configSave();
+    result = STATUS_OK;
+    }
+  return result;
+  }
+
+//---------------------------------------------------------------------------
 // Main program
 //---------------------------------------------------------------------------
 
 /** Program entry point
  */
 int main() {
-  uint16_t now;
+  uint16_t now, command;
   // Initialise libraries
   configInit();
   adcInit();
@@ -101,6 +146,11 @@ int main() {
     // Update sensor values
     configWrite(STATE_POWER, adcVoltage());
     configWrite(STATE_MOTION, adcMotion());
+    // Check for any commands and respond to them
+    if(uartRecv(&command))
+      uartSend(processCommand(command));
+    // Evaluate the current state
+
     while(tickElapsed(now)<10);
     ledState(false);
     while(tickElapsed(now)<20);
