36bdcd4399308a8a7c4c998bafc2e2602c998cf0
shane <shane@thegaragelab.com>
2014-03-29 17:58:43 +1000

    Simple script to generate a 'gource' video of project activity.

---
+++ mkgource.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#-----------------------------------------------------------
+# Create a 'gource' video from the repository
+#-----------------------------------------------------------
+
+PROJECT=nightlight
+#gource -1280x720 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ${PROJECT}.mp4
+gource -704x480 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ${PROJECT}.mp4
